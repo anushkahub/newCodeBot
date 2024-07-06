89db205b7123afccb5608209eb9506502c1fe817
Anushka Walvekar <154654286+anushkahub@users.noreply.github.com>
2024-07-06 20:28:44 +0530

    Add files via upload
    This file includes Python hello programme

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("Hello world, \nthis is Anushka!!")
+# this should print hello world #comment
+
+'''print("hey there,this is print statement 2")
+print("statement 3")'''
+name = "John"
+roll no = "14"
